be4a1f568ed49be43fb1fbd11e796c1622b0422e
Mercy Oyelude <mercyoyelude0@gmail.com>
2024-01-28 01:40:31 +0100

    Updated images.

--- src/Components/Inspiration.js
+++ src/Components/Inspiration.js
@@ -11,22 +11,22 @@ export default function Inspiration() {
             <table className='table'>
                 <tr>
                     <td>
-                        <img src={process.env.PUBLIC_URL + './images/ikeja.jpg'} alt='Ikeja-picture' className='img-fluid ' />
+                        <img src={process.env.PUBLIC_URL + './images/orlando hotel.png'} alt='Orlando-picture' className='img-fluid ' />
                         <p className='h6'>The 6 best Orlando hotels for families</p>
                         <p className='card-text'>Discover the best Orlando hotels for families for your vacation.</p>
                     </td>
                     <td>
-                        <img src={process.env.PUBLIC_URL + './images/lagos.jpg'} alt='Lagos-picture' className='img-fluid ' />
+                        <img src={process.env.PUBLIC_URL + './images/skii towns.png'} alt='Skii-picture' className='img-fluid ' />
                         <p className='h6'>5 best ski towns around the world</p>
                         <p className='card-text'>Discover a winter wonderland in these charming ski destinations.</p>
                     </td>
                     <td>
-                        <img src={process.env.PUBLIC_URL + './images/abuja.jpg'} alt='Abuja-picture' className='img-fluid ' />
+                        <img src={process.env.PUBLIC_URL + './images/vacation homes.png'} alt='Vacation-picture' className='img-fluid ' />
                         <p className='h6'>5 vacation homes for a Thanksgiving getaway</p>
                         <p className='card-text'>Enjoy Thanksgiving dinner at these vacation homes.</p>
                     </td>
                     <td>
-                        <img src={process.env.PUBLIC_URL + './images/lekki.jpg'} alt='Lekki-picture' className='img-fluid ' />
+                        <img src={process.env.PUBLIC_URL + './images/bangkok.png'} alt='Bangkok-picture' className='img-fluid ' />
                         <p className='h6'>6 incredible Bangkok rooftop bars</p>
                         <p className='card-text'>Amazing city views, cocktails, and world-class cuisine.</p>
                     </td>
