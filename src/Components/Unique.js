f3fb80ebeb902fa8b076f5d3c2f4f100dab6f81a
Mercy Oyelude <mercyoyelude0@gmail.com>
2024-01-28 11:58:52 +0100

    Updated CSS for Inspiration Section.

--- src/Components/Unique.js
+++ src/Components/Unique.js
@@ -7,48 +7,50 @@ export default function Unique() {
                 <p className='h3'>Stay at our top unique properties</p>
                 <p className='h6'>From castles and villas to boats and igloos, we have it all</p>
             </div>
-            <div classname='row flex'>
-                <div classname='col-md-3'>
-                    <div className='card'>
-                        <img src={process.env.PUBLIC_URL + './images/ikeja.jpg'} alt='Ikeja-picture' className='img-fluid ' />
-                        <div className='card-body'>
-                            <p className='h4 card-title'>Gyttja Västergårds</p>
-                            <p className='card-text'>Finland, Lillandet</p>
-                            <p className='card-text'> <span>9.3</span>Wonderful · 207 reviews</p>
+            <table className='table'>
+                <tr>
+                    <td>
+                        <div className='card'>
+                            <img src={process.env.PUBLIC_URL + './images/finland.png'} alt='Finland-picture' className='img-fluid ' />
+                            <div className='card-body'>
+                                <p className='h4 card-title'>Gyttja Västergårds</p>
+                                <p className='card-text'>Finland, Lillandet</p>
+                                <p className='card-text'> <span>9.3</span>Wonderful · 207 reviews</p>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div classname='col-md-3'>
-                    <div className='card'>
-                        <img src={process.env.PUBLIC_URL + './images/ikeja.jpg'} alt='Ikeja-picture' className='img-fluid ' />
-                        <div className='card-body'>
-                            <p className='h4 card-title'>Waldschenke Stendenitz Übernachten im Wald am See</p>
-                            <p className='card-text'>Germany, Neuruppin</p>
-                            <p className='card-text'> <span>8.7</span>Excellent · 307 reviews</p>
+                    </td>
+                    <td>
+                        <div className='card'>
+                            <img src={process.env.PUBLIC_URL + './images/germany.png'} alt='Germany-picture' className='img-fluid ' />
+                            <div className='card-body'>
+                                <p className='h4 card-title'>Waldschenke Stendenitz Übernachten im Wald am See</p>
+                                <p className='card-text'>Germany, Neuruppin</p>
+                                <p className='card-text'> <span>8.7</span>Excellent · 307 reviews</p>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div classname='col-md-3'>
-                    <div className='card'>
-                        <img src={process.env.PUBLIC_URL + './images/ikeja.jpg'} alt='Ikeja-picture' className='img-fluid ' />
-                        <div className='card-body'>
-                            <p className='h4 card-title'>Harbor View</p>
-                            2                     <p className='card-text'>United Kingdom, Isle Of Skye</p>
-                            <p className='card-text'> <span>9.0</span>Wonderful · 144 reviews</p>
+                    </td>
+                    <td>
+                        <div className='card'>
+                            <img src={process.env.PUBLIC_URL + './images/harbour view.png'} alt='Harbour-picture' className='img-fluid ' />
+                            <div className='card-body'>
+                                <p className='h4 card-title'>Harbor View</p>
+                                <p className='card-text'>United Kingdom, Isle Of Skye</p>
+                                <p className='card-text'> <span>9.0</span>Wonderful · 144 reviews</p>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div classname='col-md-3'>
-                    <div className='card'>
-                        <img src={process.env.PUBLIC_URL + './images/ikeja.jpg'} alt='Ikeja-picture' className='img-fluid ' />
-                        <div className='card-body'>
-                            <p className='h4 card-title'>Casa rural La Marquesa - Cuenca</p>
-                            <p className='card-text'>Spain, Valera de Abajo</p>
-                            <p className='card-text'> <span>9.4</span>Wonderful · 57 reviews</p>
+                    </td>
+                    <td>
+                        <div className='card'>
+                            <img src={process.env.PUBLIC_URL + './images/spain.png'} alt='Spain-picture' className='img-fluid ' />
+                            <div className='card-body'>
+                                <p className='h4 card-title'>Casa rural La Marquesa - Cuenca</p>
+                                <p className='card-text'>Spain, Valera de Abajo</p>
+                                <p className='card-text'> <span>9.4</span>Wonderful · 57 reviews</p>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            </div>
-        </div >
+                    </td>
+                </tr>
+            </table>
+        </div>
     )
 }
