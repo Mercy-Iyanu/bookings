5012388302cf9fdb95e861528a401c803d1ab14b
Mercy Oyelude <mercyoyelude0@gmail.com>
2024-02-08 00:23:03 +0100

    Updated

--- src/Components/FooterLink.js
+++ src/Components/FooterLink.js
@@ -2,10 +2,15 @@ import React from 'react';
 import '../App.css';
 import styled from 'styled-components';
 
+const StyledLink = styled.div`
+    text-decoration: none;
+`;
+
+
 export default function FooterLink({ text }) {
     return (
-        <div>
+        <StyledLink>
             <a href='#'>{text || 'Countries'}</a>
-        </div>
+        </StyledLink>
     )
 }
